b6d629995781469d525511a97201ca7289307375
Gleb B <gleb.bortsov79@gmail.com>
2023-02-28 19:33:52 +1000

    Add:
    *slingshot

--- Assets/Scripts/Player/Player/Player_Slingshot.cs
+++ Assets/Scripts/Player/Player/Player_Slingshot.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 using Players;
 using DG.Tweening;
 
@@ -8,11 +9,12 @@ namespace Character.Slingshot
     public class Player_Slingshot : MonoBehaviour
     {
         [Header("Parameters")]
-        [SerializeField] private float _maxSlingDistance = 2f;
-
+        [SerializeField] private float _maxMoveDistance = 2f;
         [SerializeField] private float _movingTime = 1f;
 
         [Header("Private")]
+        [SerializeField] private bool _isMoving = false;
+
         private Vector3 _directionMove = Vector3.zero;
 
         private Joystick _slingJoystick = null;
@@ -20,27 +22,13 @@ namespace Character.Slingshot
 
         #region MONO
 
-        private void Awake()
-        {
-            _slingJoystick = FindObjectOfType<Joystick>();
-        }
+        private void Awake() => _slingJoystick = FindObjectOfType<Joystick>();
 
-        private void Update()
-        {
-            OnSlinging();
-        }
+        private void Update() => OnSlinging();
 
-        private void OnEnable()
-        {
-            //DynamicJoystick.OnStartingGame += OnSlinging; //Оттягивание
-            DynamicJoystick.OnStartGame += OnMoving;
-        }
+        private void OnEnable() => DynamicJoystick.OnStartGame += OnMoving;
 
-        private void OnDisable()
-        {
-            //DynamicJoystick.OnStartingGame -= OnSlinging;
-            DynamicJoystick.OnStartGame -= OnMoving;
-        }
+        private void OnDisable() => DynamicJoystick.OnStartGame -= OnMoving;
 
         #endregion
 
@@ -56,13 +44,28 @@ namespace Character.Slingshot
 
         private void OnMoving()
         {
-            float newXPosition = gameObject.transform.position.x + (_maxSlingDistance * _directionMove.x);
-            float newZPosition = gameObject.transform.position.z + (_maxSlingDistance * _directionMove.z);
+            if (_isMoving == true) return;
+
+            _isMoving = true;
+
+            float newXPosition = gameObject.transform.position.x + (_maxMoveDistance * _directionMove.x);
+            float newZPosition = gameObject.transform.position.z + (_maxMoveDistance * _directionMove.z);
 
             Vector3 newPosition = new Vector3(newXPosition, gameObject.transform.position.y, newZPosition);
 
             gameObject.transform.DOMove(newPosition, _movingTime);
+
+            StartCoroutine(IsMovingCoroutine());
         }
+
+        private IEnumerator IsMovingCoroutine()
+        {
+            yield return new WaitForSeconds(_movingTime);
+
+            _isMoving = false;
+
+            yield break;
+        } 
 
         #endregion
     }
