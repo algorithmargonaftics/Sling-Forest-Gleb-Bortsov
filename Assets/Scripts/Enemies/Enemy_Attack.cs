ebadd9ce186fb686a2e91c9f89d675f9dd21b250
Gleb B <gleb.bortsov79@gmail.com>
2023-03-01 13:34:47 +1000

    Add:
    * enemy destroy effect

--- Assets/Scripts/Enemies/Enemy_Attack.cs
+++ Assets/Scripts/Enemies/Enemy_Attack.cs
@@ -4,9 +4,18 @@ namespace Enemies
 {
     public class Enemy_Attack : MonoBehaviour
     {
+        #region CONSTS
+
+        private const float Y_POSITION = 1f;
+
+        #endregion
+
         [Header("Health")]
         [SerializeField] private int _maxHealth = 1;
         private int _currentHealth = 0;
+        [Space (height: 5f)]
+
+        [SerializeField] private ParticleSystem _destroyEffect = null;
 
 
         #region MONO
@@ -27,9 +36,10 @@ namespace Enemies
 
         private void DestroyEnemy()
         {
-            Debug.Log("Destroy");
-
-            //effect
+            Instantiate(
+                _destroyEffect,
+                new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + Y_POSITION, gameObject.transform.position.z),
+                Quaternion.identity);
 
             Destroy(gameObject);
         }
